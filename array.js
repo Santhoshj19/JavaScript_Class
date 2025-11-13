31d71ea190a92a7f2b0fd6bc6aa139e097300199
Santhoshj19 <santhoshj.yrsk@gmail.com>
2025-11-13 11:19:16 +0530

    Added

---
+++ array.js
@@ -0,0 +1,6 @@
+let array = [1, 2, 3, 4, 5];
+console.log(array);
+array.unshift(6, 7, 8);
+console.log(array);
+array.shift();
+console.log(array);
---
+++ string_code.js
@@ -0,0 +1,11 @@
+// let text="heloo";
+// console.log(text.toLocaleUpperCase());
+// console.log(text.toLocaleLowerCase());
+// console.log(text.repeat(3));
+// console.log(text.trim());
+// console.log(text.trimStart());
+
+
+let text1="I am in bangalore";
+console.log(text1.search("r"));
+console.log(text1.split("").join(""));
