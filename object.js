c8451792552821680aa754a7ba3cd9a664f6c7e4
Santhoshj19 <santhoshj.yrsk@gmail.com>
2025-11-19 10:05:12 +0530

    object.js file modified

--- object.js
+++ object.js
@@ -1,6 +1,47 @@
-let mobile = {
-  Brand: "Vivo",
-  Price: 20000
-};
-console.log(mobile);
-console.log(mobile.Brand);
+// let mobile = {
+//   Brand: "Vivo",
+//   Price: 20000
+// };
+// console.log(mobile);
+// console.log(mobile.Brand);
+
+// let response = [
+//   {
+//     name: "iphone11",
+//     company: "Apple",
+//   },
+//   {
+//     name: "iphone14",
+//     comapny: "Apple",
+//   },
+//   {
+//     name: "Galaxy",
+//     comapny: "Samsung",
+//   },
+// ];
+// for (let i = 0; i <= response.length; i++) {
+//   if(response[i].company == "Apple") {
+//   }
+// }
+
+// let obj = {
+//   name: "Galaxy",
+//   company: "Samsung",
+// };
+// let obj1 = {
+//   name: "Iphone",
+//   company: "apple",
+// };
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.assign(obj, obj1));
+
+//Hoisting - moving declaration of the function to the top
+// abc();
+// function abc() {
+//   console.log("function");
+// }
+
+console.log("10" * "5");
+console.log(typeof String(123));
+
